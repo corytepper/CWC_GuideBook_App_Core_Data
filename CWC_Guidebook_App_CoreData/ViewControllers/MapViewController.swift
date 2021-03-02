475503062db8670741797a7baa061c736109c0f0
Cory Tepper <corytepper@Corys-MacBook-Pro.local>
2021-03-02 13:38:04 -0800

    add map pin and zoom to mapViewControlelr and added fetched info for infoViewController

--- CWC_Guidebook_App_CoreData/ViewControllers/MapViewController.swift
+++ CWC_Guidebook_App_CoreData/ViewControllers/MapViewController.swift
@@ -6,8 +6,18 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
+    
+    // MARK: - Variables and Properties
+
+    @IBOutlet var mapView: MKMapView!
+    
+    var place:Place?
+    
+    // MARK: - ViewController Lifecycle
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,15 +25,25 @@ class MapViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        if place != nil {
+            
+            // Create a CLLoactionCoordinate2D
+            let location = CLLocationCoordinate2D(latitude: place!.lat, longitude: place!.long)
+            
+            // Create a pin
+            let pin = MKPointAnnotation()
+            pin.coordinate = location
+            
+            // Add it to the map
+            mapView.addAnnotation(pin)
+            
+            // Create a region to zoom to
+            let region = MKCoordinateRegion(center: location, latitudinalMeters: 100, longitudinalMeters: 100)
+            
+            mapView.setRegion(region, animated: false)
+            
+        }
     }
-    */
-
 }
--- CWC_Guidebook_App_CoreData/ViewControllers/PlaceViewController.swift
+++ CWC_Guidebook_App_CoreData/ViewControllers/PlaceViewController.swift
@@ -62,10 +62,10 @@ class PlaceViewController: UIViewController {
         // Set the name
         placeNameLabel.text = place?.name
         
-       
+       // Make sure the first segment is displayed
+        segmentChanged(self.segmentedControl)
         
-        switchChildViewControllers(infoViewController)
-      
+        
     }
     
     
@@ -93,12 +93,16 @@ class PlaceViewController: UIViewController {
         
         switch sender.selectedSegmentIndex {
         case 0:
+            infoViewController.place = self.place
             switchChildViewControllers(infoViewController)
         case 1:
+            mapViewController.place = self.place
             switchChildViewControllers(mapViewController)
         case 2:
+            notesViewController.place = self.place
             switchChildViewControllers(notesViewController)
         default:
+            infoViewController.place = self.place
             switchChildViewControllers(infoViewController)
         }
         
